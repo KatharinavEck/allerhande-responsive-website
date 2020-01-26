6f9792f941c88df19328d8bc0c8e97aed4f646f3
KatharinavEck <58591336+KatharinavEck@users.noreply.github.com>
2020-01-26 14:22:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ik heb deze tutorial gebruikt en aangepast om deze code te schrijven
+//  https://itnext.io/how-to-build-a-responsive-navbar-using-flexbox-and-javascript-eb0af24f19bf
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
